be59664ef6efc6c70f9338d7a2287269e1883fd7
Shihab221 <ashihab205@gmail.com>
2025-04-21 22:51:55 +0600

    details navigation started

--- app/(main)/productDetails/[id]/page.tsx
+++ app/(main)/productDetails/[id]/page.tsx
@@ -1,13 +1,20 @@
 "use client"
 import React from 'react'
 import { useParams } from 'next/navigation'
+import { topProductsData } from '@/constant';
 const page = () => {
     const params = useParams();
-    
+    const  id  = params?.id;
+    const productId = Array.isArray(id) ? id[0] : id;
+    if (!productId || isNaN(parseInt(productId, 10))) {
+      return <p>Invalid product ID</p>;
+    }
+    const product = topProductsData.find((item)=> item.id === parseInt(productId,10));
+   
   return (
     <div className='px-10 sm:px-20 min-h-screen'>
       <div className='text-neutral-900'>
-        <p></p>
+        <p>{product.title}</p>
       </div>
     </div>
   )

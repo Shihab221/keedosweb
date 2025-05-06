3839208fc211084e84990d070abfe9b343438601
Shihab221 <ashihab205@gmail.com>
2025-05-06 12:23:25 +0600

    upload page initilized

---
+++ app/(main)/uploadProduct/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='text-neutral-900 px-5 sm:px-20 py-10'>
+      <h4 className='text-xl font-semibold'>Welcome Admin , to upload page </h4>
+    </div>
+  )
+}
+
+export default page

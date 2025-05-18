d0ab6961054d0263f2f40ef5dd186ae6445d1259
Shihab221 <ashihab205@gmail.com>
2025-05-19 02:31:26 +0600

    best selling added

--- components/bestSelling.tsx
+++ components/bestSelling.tsx
@@ -3,9 +3,10 @@ import { ProductsData } from '@/constant'
 import HorizontalCard from './horizontalCard'
 const BestSelling = () => {
   return (
-    <div>
-      <h4>Best Selling</h4>
-      <div>
+    <div className='px-10 sm:px-20 py-10 my-10'>
+      <h4 className='text-2xl text-neutral-900 font-semibold pb-4'>Best Selling</h4>
+      <div className='
+      flex flex-col sm:flex-row gap-4 items-center justify-between flex-wrap'>
         {ProductsData.map((item)=>(
             <HorizontalCard 
             {...item}
--- components/horizontalCard.tsx
+++ components/horizontalCard.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import Image from 'next/image';
+import { FaStar } from 'react-icons/fa';
 export interface ProductCardProps {
   id:number,
   title: string;
@@ -16,20 +17,29 @@ export interface ProductCardProps {
 const HorizontalCard = ({title,price,reviews, image, discount}:ProductCardProps) => {
   return (
     <motion.div 
-    className='flex flex-col items-start rounded-lg text-neutral-900 '
+    className='flex flex-wrap w-fit text-neutral-900 w-[400px] '
     initial={{opacity:0 , y:50}}
     whileInView={{opacity:1 , y:0}}
     transition={{duration:0.3}}
     viewport={{once:true}}
     >
-      <div className='flex flex-row justify-between items-center w-full bg-neutral-100 px-2 '>
+      <div className='flex flex-row  items-center w-full bg-neutral-100 px-2 text-neutral-900 w-sm'>
         <Image 
             src={image}
             alt='product_image'
             width={100}
             height={100}
-            className='object-contain w-[300px] h-[280px] p-4 bg-neutral-100'
+            className='object-contain w-24 h-24 p-4 bg-neutral-100'
         />
+        <div className='flex flex-col gap-2'>
+          <p>{title}</p>
+          <div className='flex flex-row items-center'>
+          {[...Array(5)].map((_, i) => (
+                    <FaStar key={i} size={14} className='text-sky-500'/>
+                  ))}
+          <p className='text-xs px-1'>({reviews})</p>
+          </div>
+        </div>
       </div>
     </motion.div>
   )
